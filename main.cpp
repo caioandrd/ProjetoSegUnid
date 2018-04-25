5ac8827205f7b2ee5821bb051075da19c79dc817
Caio Andrade <38655994+caioandrd@users.noreply.github.com>
2018-04-25 00:56:14 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,6 @@ int main()
    Point p(-11,2), p1(3,4), p2, p3, t;
    Poligono pol;
    Retangulo ret(0,0,4,3);
-   float a = 5;
-   float b = 6;
-   float c = -3;
-   float d = 4;
 
    cout << "Ponto  P: "; p.imprime(); cout << endl;
    cout << "Ponto  P1: "; p1.imprime(); cout << endl;
@@ -22,14 +18,26 @@ int main()
    p3 = p.sub(p1);
    cout << "Ponto  P - P1: "; p3.imprime(); cout << endl;
    cout << "Norma ponto P: "; cout << p.norma(); cout << endl;
-   t = p.translada(a,b);
-   cout << "Ponto P transladado de (" <<
-           a << "," << b << "): "; t.imprime(); cout << endl << endl;
+   t = p.translada(5,6);
+   cout << "Ponto P transladado de (5,6): ";
+   t.imprime(); cout << endl << endl;
+
+   pol.addvertices(0,0);
+   pol.addvertices(2,1);
+   pol.addvertices(1,3);
+   pol.addvertices(-2,2);
+   pol.addvertices(-1,1);
+   pol.imprimep();
+   cout << "Quantidade de vertices do poligono: " << pol.qtdvertices() << endl;
+   cout << "Area do poligono: " << pol.area() << endl;
+   cout << "Poligono  transladado de (5,6): ";
+   pol.transladap(5,6);
+   pol.imprimep(); cout << endl << endl;
 
    ret.imprimep();
-   cout << "Area do retangulo: " << ret.area() << endl << endl;
-   ret.transladap(c,d);
-      cout << "Retangulo transladado de (" <<
-           c << "," << d << "): "; ret.imprimep();
-   cout << "Area transladada: " << ret.area(); cout << endl;
+   cout << "Area do retangulo: " << ret.area() << endl;
+   ret.transladap(-3,4);
+   cout << "Retangulo transladado de (-3,4): ";
+   ret.imprimep();
+   cout << "Area transladada: " << ret.area(); cout << endl << endl;
   }
